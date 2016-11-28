e5a7cfbe1a5a03f830f31d38f2101baf892192a6
Nathan <avaglir@gmail.com>
2016-11-27 21:43:52 -0500

    drop scala

--- build.sbt
+++
@@ -1,4 +0,0 @@
-libraryDependencies ++= Seq(
-  "com.skadistats" % "clarity" % "2.1",
-  "com.github.nscala-time" %% "nscala-time" % "2.14.0"
-)
--- retrieve/parse.py
+++ retrieve/parse.py
@@ -1,21 +1,44 @@
 import os
 import json
+import csv
 
-data = {f: json.load(f) for f in os.listdir('../replays/data')}
+data = {}
+for match in os.listdir('../replays/data'):
+    with open('../replays/data/' + match) as f:
+        data[match] = json.load(f)
+
 out = {}
 
 for match, info in data.items():
-    rosh_killed = [elem for elem in data['objectives'] if elem['type'] == 'CHAT_MESSAGE_ROSHAN_KILL']
-    killer = min(rosh_killed, key=lambda x: x['time'])['player_slot']
+    rosh_killed = [elem for elem in info['objectives'] if elem['type'] == 'CHAT_MESSAGE_ROSHAN_KILL']
 
-    radiant_first_rosh = False
-    for player in data['players']:
-        if player['player_slot'] == killer:
-            radiant_first_rosh = player['isRadiant']
+    if rosh_killed == []:
+        radiant_first_rosh = None
+        killer = None
+    else:
+        killer = min(rosh_killed, key=lambda x: x['time'])['team']
 
-    dire_obs_count = sum([player['obs_placed'] for player in data['players'] if not player['isRadiant']])
-    radiant_obs_count = sum([player['obs_placed'] for player in data['players'] if player['isRadiant']])
+    radiant_first_rosh = killer == 2
 
-    radiant_win = data['radiant_win']
+    dire_obs_count = sum([player['observer_uses'] for player in info['players'] if not player['isRadiant'] and player['observer_uses']])
+    radiant_obs_count = sum([player['observer_uses'] for player in info['players'] if player['isRadiant'] and player['observer_uses']])
 
-    radiantLeads = data['radiant_gold_adv']
+    radiant_win = info['players'][0]['radiant_win']
+
+    adv = info['radiant_gold_adv']
+    radiantLeads = info['radiant_gold_adv'][10]
+
+    out[match] = {
+        'winner': 'radiant' if radiant_win else 'dire',
+        'dire_obs': dire_obs_count,
+        'radiant_obs': radiant_obs_count,
+        'first_roshan': None if not killer else ('radiant' if radiant_first_rosh else 'dire'),
+        'gl_10': adv[10] if len(adv) > 10 else None,
+        'gl_20': adv[20] if len(adv) > 20 else None,
+        'gl_30': adv[30] if len(adv) > 30 else None
+    }
+
+with open('ti6.csv', 'w', newline='') as f:
+    w = csv.DictWriter(f, ['winner', 'dire_obs', 'radiant_obs', 'first_roshan', 'gl_10', 'gl_20', 'gl_30'])
+    w.writeheader()
+    w.writerows(out.values())
